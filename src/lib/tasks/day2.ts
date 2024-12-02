2bc3fab11c06823af00cc17034f61b1c64d470af
Henry <henry@gedert.de>
2024-12-02 20:47:44 +0100

    it worked by brute force 😆

--- src/lib/tasks/day2.ts
+++ src/lib/tasks/day2.ts
@@ -5,96 +5,83 @@ enum Direction {
 
 export async function task1(input: string): Promise<string> {
 	const reportList = preprocessor(input);
-
 	let counter = 0;
-	reportIter: for (let i = 0; i < reportList.length; i++) {
-		const report = reportList[i];
-		let direction: Direction;
 
-		if (report.at(0)! < report.at(1)!) {
-			direction = Direction.increasing;
+	for (let i = 0; i < reportList.length; i++) {
+		if (validateReport(reportList[i])) {
+			counter++;
 		}
-		if (report.at(0)! > report.at(1)!) {
-			direction = Direction.decreasing;
-		}
-		if (report.at(0)! === report.at(1)!) {
-			continue reportIter;
-		}
-
-		for (let j = 0; j < report.length - 1; j++) {
-			const currentLevel = report[j];
-			const nextLevel = report[j + 1];
-			const diff = Math.abs(currentLevel - nextLevel);
-
-			if (3 < diff) {
-				continue reportIter;
-			}
-
-			switch (direction!) {
-				case Direction.increasing:
-					if (currentLevel >= nextLevel) {
-						continue reportIter;
-					}
-					break;
-				case Direction.decreasing:
-					if (currentLevel <= nextLevel) {
-						continue reportIter;
-					}
-					break;
-			}
-		}
-		counter++;
 	}
+
 	return counter.toString();
 }
 
 export async function task2(input: string): Promise<string> {
 	const reportList = preprocessor(input);
-
 	let counter = 0;
-	reportIter: for (let i = 0; i < reportList.length; i++) {
-		const report = reportList[i];
-		let direction: Direction;
+	const naughtyList: number[][] = [];
 
-
-		if (report.at(0)! < report.at(-1)!) {
-			direction = Direction.increasing;
+	// Search for pure good lists :)
+	for (const report of reportList) {
+		if (validateReport(report)) {
+			counter++;
+		} else {
+			naughtyList.push(report);
 		}
-		if (report.at(0)! > report.at(-1)!) {
-			direction = Direction.decreasing;
-		}
-		if (report.at(0)! === report.at(1)!) {
-			continue reportIter;
-		}
-
-		for (let j = 0; j < report.length - 1; j++) {
-			const currentLevel = report[j];
-			const nextLevel = report[j + 1];
-			const diff = Math.abs(currentLevel - nextLevel);
-
-			if (3 < diff) {
-				continue reportIter;
-			}
-
-			switch (direction!) {
-				case Direction.increasing:
-					if (currentLevel >= nextLevel) {
-						continue reportIter;
-					}
-					break;
-				case Direction.decreasing:
-					if (currentLevel <= nextLevel) {
-						continue reportIter;
-					}
-					break;
-			}
-		}
-		counter++;
 	}
+
+
+	// Search for the kinda good lists ?
+	reportItr: for (const report of naughtyList) {
+		for (let i = 0; i < report.length; i++) {
+			const maybeFixedReport = report.toSpliced(i, 1);
+			if (validateReport(maybeFixedReport)) {
+				counter++;
+				continue reportItr;
+			}
+		}
+	}
+
 	return counter.toString();
 }
 
-// function validateReport(report: number[]): boolean {}
+function validateReport(report: number[]): boolean {
+	let direction: Direction;
+
+	if (report.at(0)! < report.at(1)!) {
+		direction = Direction.increasing;
+	}
+	if (report.at(0)! > report.at(1)!) {
+		direction = Direction.decreasing;
+	}
+	if (report.at(0)! === report.at(1)!) {
+		return false;
+	}
+
+	for (let j = 0; j < report.length - 1; j++) {
+		const currentLevel = report[j];
+		const nextLevel = report[j + 1];
+		const diff = Math.abs(currentLevel - nextLevel);
+
+		if (3 < diff) {
+			return false;
+		}
+
+		switch (direction!) {
+			case Direction.increasing:
+				if (currentLevel >= nextLevel) {
+					return false;
+				}
+				break;
+			case Direction.decreasing:
+				if (currentLevel <= nextLevel) {
+					return false;
+				}
+				break;
+		}
+	}
+	return true;
+}
 
 function preprocessor(input: string): number[][] {
 	return input.split('\n').map((report) => report.split(' ').map((levels) => Number(levels)));
