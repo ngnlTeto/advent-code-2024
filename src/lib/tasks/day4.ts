adc1dbf85ce1efb38c1cd38ea55615cfea566724
Henry <henry@gedert.de>
2024-12-05 21:05:24 +0100

    optimized day 4

--- src/lib/tasks/day4.ts
+++ src/lib/tasks/day4.ts
@@ -1,71 +1,85 @@
 export async function task1(input: string): Promise<string> {
 	const xmasMat = preprocessor(input);
 	let counter = 0;
+	const iLength = xmasMat.length;
+	const jLength = xmasMat[0].length;
 
-	for (let i = 0; i < xmasMat.length; i++) {
-		for (let j = 0; j < xmasMat[0].length; j++) {
+	for (let i = 0; i < iLength; i++) {
+		for (let j = 0; j < jLength; j++) {
 			if (xmasMat[i][j] === 'X') {
 				// North
 				if (
-					(xmasMat[i - 1] ?? [])[j] === 'M' &&
-					(xmasMat[i - 2] ?? [])[j] === 'A' &&
-					(xmasMat[i - 3] ?? [])[j] === 'S'
+					2 < i &&
+					xmasMat[i - 1][j] === 'M' &&
+					xmasMat[i - 2][j] === 'A' &&
+					xmasMat[i - 3][j] === 'S'
 				) {
 					counter++;
 				}
 				// North-East
 				if (
-					(xmasMat[i - 1] ?? [])[j + 1] === 'M' &&
-					(xmasMat[i - 2] ?? [])[j + 2] === 'A' &&
-					(xmasMat[i - 3] ?? [])[j + 3] === 'S'
+					2 < i &&
+					j < jLength - 3 &&
+					xmasMat[i - 1][j + 1] === 'M' &&
+					xmasMat[i - 2][j + 2] === 'A' &&
+					xmasMat[i - 3][j + 3] === 'S'
 				) {
 					counter++;
 				}
 				// East
 				if (
-					(xmasMat[i] ?? [])[j + 1] === 'M' &&
-					(xmasMat[i] ?? [])[j + 2] === 'A' &&
-					(xmasMat[i] ?? [])[j + 3] === 'S'
+					j < jLength - 3 &&
+					xmasMat[i][j + 1] === 'M' &&
+					xmasMat[i][j + 2] === 'A' &&
+					xmasMat[i][j + 3] === 'S'
 				) {
 					counter++;
 				}
 				// South-East
 				if (
-					(xmasMat[i + 1] ?? [])[j + 1] === 'M' &&
-					(xmasMat[i + 2] ?? [])[j + 2] === 'A' &&
-					(xmasMat[i + 3] ?? [])[j + 3] === 'S'
+					j < jLength - 3 &&
+					i < iLength - 3 &&
+					xmasMat[i + 1][j + 1] === 'M' &&
+					xmasMat[i + 2][j + 2] === 'A' &&
+					xmasMat[i + 3][j + 3] === 'S'
 				) {
 					counter++;
 				}
 				// South
 				if (
-					(xmasMat[i + 1] ?? [])[j] === 'M' &&
-					(xmasMat[i + 2] ?? [])[j] === 'A' &&
-					(xmasMat[i + 3] ?? [])[j] === 'S'
+					i < iLength - 3 &&
+					xmasMat[i + 1][j] === 'M' &&
+					xmasMat[i + 2][j] === 'A' &&
+					xmasMat[i + 3][j] === 'S'
 				) {
 					counter++;
 				}
 				// South-West
 				if (
-					(xmasMat[i + 1] ?? [])[j - 1] === 'M' &&
-					(xmasMat[i + 2] ?? [])[j - 2] === 'A' &&
-					(xmasMat[i + 3] ?? [])[j - 3] === 'S'
+					i < iLength - 3 &&
+					2 < j &&
+					xmasMat[i + 1][j - 1] === 'M' &&
+					xmasMat[i + 2][j - 2] === 'A' &&
+					xmasMat[i + 3][j - 3] === 'S'
 				) {
 					counter++;
 				}
 				// West
 				if (
-					(xmasMat[i] ?? [])[j - 1] === 'M' &&
-					(xmasMat[i] ?? [])[j - 2] === 'A' &&
-					(xmasMat[i] ?? [])[j - 3] === 'S'
+					2 < j &&
+					xmasMat[i][j - 1] === 'M' &&
+					xmasMat[i][j - 2] === 'A' &&
+					xmasMat[i][j - 3] === 'S'
 				) {
 					counter++;
 				}
 				// North-West
 				if (
-					(xmasMat[i - 1] ?? [])[j - 1] === 'M' &&
-					(xmasMat[i - 2] ?? [])[j - 2] === 'A' &&
-					(xmasMat[i - 3] ?? [])[j - 3] === 'S'
+					2 < j &&
+					2 < i &&
+					xmasMat[i - 1][j - 1] === 'M' &&
+					xmasMat[i - 2][j - 2] === 'A' &&
+					xmasMat[i - 3][j - 3] === 'S'
 				) {
 					counter++;
 				}
@@ -80,17 +94,17 @@ export async function task2(input: string): Promise<string> {
 	const xmasMat = preprocessor(input);
 	let counter = 0;
 
-	for (let i = 0; i < xmasMat.length; i++) {
-		for (let j = 0; j < xmasMat[0].length; j++) {
+	for (let i = 1; i < xmasMat.length - 1; i++) {
+		for (let j = 1; j < xmasMat[0].length - 1; j++) {
 			if (xmasMat[i][j] === 'A') {
 				// M.S
 				// .A.
 				// M.S
 				if (
-					(xmasMat[i - 1] ?? [])[j - 1] === 'M' &&
-					(xmasMat[i - 1] ?? [])[j + 1] === 'S' &&
-					(xmasMat[i + 1] ?? [])[j - 1] === 'M' &&
-					(xmasMat[i + 1] ?? [])[j + 1] === 'S'
+					xmasMat[i - 1][j - 1] === 'M' &&
+					xmasMat[i - 1][j + 1] === 'S' &&
+					xmasMat[i + 1][j - 1] === 'M' &&
+					xmasMat[i + 1][j + 1] === 'S'
 				) {
 					counter++;
 				}
@@ -98,10 +112,10 @@ export async function task2(input: string): Promise<string> {
 				// .A.
 				// S.S
 				if (
-					(xmasMat[i - 1] ?? [])[j - 1] === 'M' &&
-					(xmasMat[i - 1] ?? [])[j + 1] === 'M' &&
-					(xmasMat[i + 1] ?? [])[j - 1] === 'S' &&
-					(xmasMat[i + 1] ?? [])[j + 1] === 'S'
+					xmasMat[i - 1][j - 1] === 'M' &&
+					xmasMat[i - 1][j + 1] === 'M' &&
+					xmasMat[i + 1][j - 1] === 'S' &&
+					xmasMat[i + 1][j + 1] === 'S'
 				) {
 					counter++;
 				}
@@ -109,10 +123,10 @@ export async function task2(input: string): Promise<string> {
 				// .A.
 				// S.M
 				if (
-					(xmasMat[i - 1] ?? [])[j - 1] === 'S' &&
-					(xmasMat[i - 1] ?? [])[j + 1] === 'M' &&
-					(xmasMat[i + 1] ?? [])[j - 1] === 'S' &&
-					(xmasMat[i + 1] ?? [])[j + 1] === 'M'
+					xmasMat[i - 1][j - 1] === 'S' &&
+					xmasMat[i - 1][j + 1] === 'M' &&
+					xmasMat[i + 1][j - 1] === 'S' &&
+					xmasMat[i + 1][j + 1] === 'M'
 				) {
 					counter++;
 				}
@@ -120,10 +134,10 @@ export async function task2(input: string): Promise<string> {
 				// .A.
 				// M.M
 				if (
-					(xmasMat[i - 1] ?? [])[j - 1] === 'S' &&
-					(xmasMat[i - 1] ?? [])[j + 1] === 'S' &&
-					(xmasMat[i + 1] ?? [])[j - 1] === 'M' &&
-					(xmasMat[i + 1] ?? [])[j + 1] === 'M'
+					xmasMat[i - 1][j - 1] === 'S' &&
+					xmasMat[i - 1][j + 1] === 'S' &&
+					xmasMat[i + 1][j - 1] === 'M' &&
+					xmasMat[i + 1][j + 1] === 'M'
 				) {
 					counter++;
 				}
