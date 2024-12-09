adc2438875ee8ecdb2cb18b9c5f8c65f58582341
Henry <henry@gedert.de>
2024-12-09 22:19:32 +0100

    solved task 2 (I hate one by of errors (what the fuck why was this "+ 1" assignment there in the first place 😠))

--- src/lib/tasks/day6.ts
+++ src/lib/tasks/day6.ts
@@ -1,8 +1,112 @@
 export async function task1(input: string): Promise<string> {
 	const map = preprocessor(input);
 
-	let rotationCount = 0;
+	putStepsOnMap(map);
 
+	let count = 0;
+	for (const row of map) {
+		count += row.filter((x) => x === 'X').length;
+	}
+
+	return Promise.resolve(count.toString());
+}
+
+export async function task2(input: string): Promise<string> {
+	const map = preprocessor(input);
+
+	const wardPos = {
+		i: map.findIndex((x) => x.includes('^')),
+		j: map[map.findIndex((x) => x.includes('^'))].indexOf('^')
+	};
+
+	putStepsOnMap(map);
+
+	map[wardPos.i][wardPos.j] = '^';
+
+	const wayPoints: { i: number; j: number }[] = [];
+	for (let i = 0; i < map.length; i++) {
+		for (let j = 0; j < map[i].length; j++) {
+			if (map[i][j] === 'X') {
+				wayPoints.push({ i, j });
+			}
+		}
+	}
+
+	let counter = 0;
+	for (const wayPoint of wayPoints) {
+		if (isPosGoodForObstacle(map, wayPoint.i, wayPoint.j)) {
+			counter++;
+		}
+	}
+
+	return Promise.resolve(counter.toString());
+}
+
+function isPosValid(map: string[][], i: number, j: number): boolean {
+	return acc(map, i, j) !== undefined;
+}
+
+function isPosGoodForObstacle(map: string[][], i: number, j: number): boolean {
+	let iPos = map.findIndex((x) => x.includes('^'));
+	let jPos = map[iPos].indexOf('^');
+	let rotationCount = 0;
+	const stepMax = map.length * map[0].length;
+	let stepCount = 0;
+
+	const newMap = JSON.parse(JSON.stringify(map));
+
+	newMap[i][j] = '#';
+
+	do {
+		//move
+		switch (rotationCount % 4) {
+			// north
+			case 0:
+				if (acc(newMap, iPos - 1, jPos) === '#') {
+					rotationCount++;
+				} else {
+					iPos--;
+					stepCount++;
+				}
+				break;
+			// east
+			case 1:
+				if (acc(newMap, iPos, jPos + 1) === '#') {
+					rotationCount++;
+				} else {
+					jPos++;
+					stepCount++;
+				}
+				break;
+			// south
+			case 2:
+				if (acc(newMap, iPos + 1, jPos) === '#') {
+					rotationCount++;
+				} else {
+					iPos++;
+					stepCount++;
+				}
+				break;
+			// west
+			case 3:
+				if (acc(newMap, iPos, jPos - 1) === '#') {
+					rotationCount++;
+				} else {
+					jPos--;
+					stepCount++;
+				}
+				break;
+		}
+
+		if (stepMax < stepCount) {
+			return true;
+		}
+	} while (isPosValid(newMap, iPos, jPos));
+	return false;
+}
+
+function putStepsOnMap(map: string[][]): void {
+	let rotationCount = 0;
 	let iPos = map.findIndex((x) => x.includes('^'));
 	let jPos = map[iPos].indexOf('^');
 
@@ -46,131 +150,6 @@ export async function task1(input: string): Promise<string> {
 				break;
 		}
 	} while (isPosValid(map, iPos, jPos));
-
-	let count = 0;
-	for (const row of map) {
-		count += row.filter((x) => x === 'X').length;
-	}
-
-	return Promise.resolve(count.toString());
-}
-
-export async function task2(input: string): Promise<string> {
-	const map = preprocessor(input);
-
-	let rotationCount = 0;
-    let goodPosCount = 0;
-
-	let iPos = map.findIndex((x) => x.includes('^'));
-	let jPos = map[iPos].indexOf('^') + 1;
-
-	do {
-		// check if obstical createsLoop
-        if (isPosGoodForObstacle(map, iPos, jPos)) {
-            goodPosCount++;
-        }
-
-		//move
-		switch (rotationCount % 4) {
-			// north
-			case 0:
-				if (acc(map, iPos - 1, jPos) === '#') {
-					rotationCount++;
-				} else {
-					iPos--;
-				}
-				break;
-			// east
-			case 1:
-				if (acc(map, iPos, jPos + 1) === '#') {
-					rotationCount++;
-				} else {
-					jPos++;
-				}
-				break;
-			// south
-			case 2:
-				if (acc(map, iPos + 1, jPos) === '#') {
-					rotationCount++;
-				} else {
-					iPos++;
-				}
-				break;
-			// west
-			case 3:
-				if (acc(map, iPos, jPos - 1) === '#') {
-					rotationCount++;
-				} else {
-					jPos--;
-				}
-				break;
-		}
-	} while (isPosValid(map, iPos, jPos));
-
-	return Promise.resolve(goodPosCount.toString());
-}
-
-function isPosValid(map: string[][], i: number, j: number): boolean {
-	return acc(map, i, j) !== undefined;
-}
-
-function isPosGoodForObstacle(map: string[][], i: number, j: number): boolean {
-	let iPos = map.findIndex((x) => x.includes('^'));
-	let jPos = map[iPos].indexOf('^') + 1;
-	let rotationCount = 0;
-	const stepMax = map.length * map[0].length;
-	let stepCount = 0;
-
-    const newMap = structuredClone(map);
-    newMap[i][j] = "#"
-
-	do {
-		//move
-		switch (rotationCount % 4) {
-			// north
-			case 0:
-				if (acc(newMap, iPos - 1, jPos) === '#') {
-					rotationCount++;
-				} else {
-					iPos--;
-                    stepCount++;
-				}
-				break;
-			// east
-			case 1:
-				if (acc(newMap, iPos, jPos + 1) === '#') {
-					rotationCount++;
-				} else {
-					jPos++;
-                    stepCount++;
-				}
-				break;
-			// south
-			case 2:
-				if (acc(newMap, iPos + 1, jPos) === '#') {
-					rotationCount++;
-				} else {
-					iPos++;
-                    stepCount++;
-				}
-				break;
-			// west
-			case 3:
-				if (acc(newMap, iPos, jPos - 1) === '#') {
-					rotationCount++;
-				} else {
-					jPos--;
-                    stepCount++;
-				}
-				break;
-		}
-
-        if (stepMax < stepCount) {
-            return true;
-        }
-	} while (isPosValid(newMap, iPos, jPos));
-
-    return false;
 }
 
 /**
