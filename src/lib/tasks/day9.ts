398e37f6d2e1bdda1c906013e94f08a200f244f4
Henry <henry@gedert.de>
2024-12-11 23:15:17 +0100

    removed isolated methods cause of the different try for task 2

--- src/lib/tasks/day9.ts
+++ src/lib/tasks/day9.ts
@@ -10,21 +10,8 @@ interface Block {
 
 export async function task1(input: string): Promise<string> {
 	const discMap = input.split('').map(Number);
-	const fileBlocks = discMapToFileBlocks(discMap);
-	const defragedFileBlocks = defragFileBlocks(fileBlocks);
-	const checksum = calcChecksum(defragedFileBlocks);
-	return Promise.resolve(checksum);
-}
 
-export async function task2(input: string): Promise<string> {
-	const discMap = input.split('').map(Number);
-	const blocks: Block[] = [];
-}
-
-//#region task 1 methods
-function discMapToFileBlocks(discMap: number[]): (string | number)[] {
 	const fileBlocks: (string | number)[] = [];
-
 	for (let i = 0; i < discMap.length; i++) {
 		if (i % 2 === 0) {
 			const id = i / 2;
@@ -37,35 +24,34 @@ function discMapToFileBlocks(discMap: number[]): (string | number)[] {
 			}
 		}
 	}
-	return fileBlocks;
-}
 
-function defragFileBlocks(fileBlocks: (string | number)[]): number[] {
-	const returnBlocks: number[] = [];
+	const defragedFileBlocks: number[] = [];
 	let left = 0;
 	let right = fileBlocks.length;
 
 	while (left < right) {
 		if (fileBlocks[left] !== '.') {
-			returnBlocks.push(fileBlocks[left] as number);
+			defragedFileBlocks.push(fileBlocks[left] as number);
 			left++;
 		} else {
 			right--;
 			if (fileBlocks[right] !== '.') {
 				left++;
-				returnBlocks.push(fileBlocks[right] as number);
+				defragedFileBlocks.push(fileBlocks[right] as number);
 			}
 		}
 	}
-	return returnBlocks;
-}
 
-function calcChecksum(fileBlocks: number[]): string {
 	let checksum = BigInt(0);
-	for (let i = 0; i < fileBlocks.length; i++) {
-		const block = fileBlocks[i];
+	for (let i = 0; i < defragedFileBlocks.length; i++) {
+		const block = defragedFileBlocks[i];
 		checksum += BigInt(i) * BigInt(block);
 	}
-	return checksum.toString();
+	return Promise.resolve(checksum.toString());
 }
-//#endregion
+
+export async function task2(input: string): Promise<string> {
+	const discMap = input.split('').map(Number);
+	const blocks: Block[] = [];
+}
+
