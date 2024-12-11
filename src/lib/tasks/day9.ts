037a5653aa73f295ca12e9ddac0e443c3fc04385
Henry <henry@gedert.de>
2024-12-12 00:04:16 +0100

    solved task 2

--- src/lib/tasks/day9.ts
+++ src/lib/tasks/day9.ts
@@ -3,7 +3,7 @@ enum BlockTypes {
 	File
 }
 interface Block {
-	id: number;
+	id: number | null;
 	space: number;
 	type: BlockTypes;
 }
@@ -52,6 +52,54 @@ export async function task1(input: string): Promise<string> {
 
 export async function task2(input: string): Promise<string> {
 	const discMap = input.split('').map(Number);
-	const blocks: Block[] = [];
-}
 
+	const blocks: Block[] = [];
+	for (let i = 0; i < discMap.length; i++) {
+		if (i % 2 === 0) {
+			blocks.push({ id: i / 2, space: discMap[i], type: BlockTypes.File });
+		} else {
+			blocks.push({ id: null, space: discMap[i], type: BlockTypes.Free });
+		}
+	}
+
+	outerloop: for (let i = blocks.length - 1; 0 <= i; i--) {
+		const block = blocks[i];
+		if (block.type === BlockTypes.File) {
+			for (let j = 0; j < i; j++) {
+				if (blocks[j].type === BlockTypes.Free && block.space <= blocks[j].space) {
+					// removes block from the back
+					blocks.splice(i, 1, { id: null, space: blocks[i].space, type: BlockTypes.Free });
+
+					// removes free space
+					blocks[j].space -= block.space;
+
+					if (blocks[j].space === 0) {
+						// replaces empty space block
+						blocks[j] = block;
+					} else {
+						// add block to the front
+						blocks.splice(j, 0, block);
+					}
+
+					continue outerloop;
+				}
+			}
+		}
+	}
+
+	let checksum = BigInt(0);
+	let id = 0;
+	for (const block of blocks) {
+		if (block.type === BlockTypes.File) {
+			for (let i = 0; i < block.space; i++) {
+				checksum += BigInt(block.id as number) * BigInt(id);
+				id++;
+			}
+		} else {
+			for (let i = 0; i < block.space; i++) {
+				id++;
+			}
+		}
+	}
+	return Promise.resolve(checksum.toString());
+}
