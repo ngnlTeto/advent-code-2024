2a863f57eeea693ed1db56657f1ff79021c1d077
Henry <henry@gedert.de>
2024-12-14 00:46:37 +0100

    Wellll, that should work... BUT, it's unbeleavably slow and I don't know how many years it needs to execute...

--- src/lib/tasks/day11.ts
+++ src/lib/tasks/day11.ts
@@ -1,3 +1,5 @@
+const markovMap: Map<bigint, { firstVal: bigint; secondVal?: bigint }> = new Map();
+
 export async function task1(input: string): Promise<string> {
 	let stones = input.split(' ').map(BigInt);
 
@@ -10,20 +12,69 @@ export async function task1(input: string): Promise<string> {
 
 export async function task2(input: string): Promise<string> {
 	const stones = input.split(' ').map(BigInt);
-  let count = 0;
+	const iterations = 75;
 
-  for await (const stone of stones) {
-    let sepStone = [stone];
-    console.count('Stone');
-    for (let i = 0; i < 75; i++) {
-      console.count('  Iterations');
+	// Build markovChain
+	for await (const stone of stones) {
+		resolveStoneToMarkovChain(stone, iterations);
+	}
 
-      sepStone = executeRules(sepStone);
-      console.log('  Stone count:', sepStone.length);
-    }
-    count += sepStone.length;
-  }
+	let count = 0n;
+	for (const stone of stones) {
+		count += stoneCount(stone, iterations);
+		console.log('Done: ', stone);
+	}
+
 	return Promise.resolve(count.toString());
+}
+
+function resolveStoneToMarkovChain(stone: bigint, maxDepth: number, curDepth: number = 0): void {
+	let newTupel: { firstVal: bigint; secondVal?: bigint };
+
+	// Rule #1
+	if (stone === 0n) {
+		newTupel = { firstVal: 1n };
+	}
+
+	// Rule #2
+	if (stone.toString().length % 2 === 0) {
+		const stoneStr = stone.toString();
+		newTupel = {
+			firstVal: BigInt(stoneStr.substring(0, stoneStr.length / 2)),
+			secondVal: BigInt(stoneStr.substring(stoneStr.length / 2, stoneStr.length))
+		};
+	} else {
+		// Rule #3
+		newTupel = { firstVal: stone * 2024n };
+	}
+
+	markovMap.set(stone, newTupel);
+	if (!markovMap.has(newTupel.firstVal) && curDepth < maxDepth) {
+		resolveStoneToMarkovChain(newTupel.firstVal, maxDepth, curDepth + 1);
+	}
+	if (
+		newTupel.secondVal !== undefined &&
+		!markovMap.has(newTupel.secondVal) &&
+		curDepth < maxDepth
+	) {
+		resolveStoneToMarkovChain(newTupel.secondVal, maxDepth, curDepth + 1);
+	}
+}
+
+function stoneCount(stone: bigint, maxDepth: number, curDepth: number = 0): bigint {
+	if (!(curDepth < maxDepth)) {
+		return 1n;
+	}
+
+	const results = markovMap.get(stone);
+	let count = 0n;
+
+	count += stoneCount(results!.firstVal, maxDepth, curDepth + 1);
+	if (results?.secondVal !== undefined) {
+		count += stoneCount(results!.secondVal, maxDepth, curDepth + 1);
+	}
+
+	return count;
 }
 
 function executeRules(stones: bigint[]): bigint[] {
